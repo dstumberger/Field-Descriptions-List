bc32a0a8e39ca31e11c6eb1c11ff017d01ed202f
Doug Stumberger <dstumberger@f1c.io>
2022-10-28 20:27:10 -0700

    Update import form

--- src/Form/FieldDescriptionsListImportForm.php
+++ src/Form/FieldDescriptionsListImportForm.php
@@ -4,6 +4,7 @@ namespace Drupal\field_descriptions_list\Form;
 
 use Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException;
 use Drupal\Component\Plugin\Exception\PluginNotFoundException;
+use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
@@ -15,7 +16,7 @@ class FieldDescriptionsListImportForm extends FormBase {
   /**
    * The Entity type manager service.
    *
-   * @var EntityTypeManagerInterface
+   * @var EntityTypeManagerInterface $entityTypeManager
    */
   protected $entityTypeManager;
 
@@ -54,6 +55,11 @@ class FieldDescriptionsListImportForm extends FormBase {
       'file_validate_extensions' => ['csv'],
     );
 
+    $caption = '<p>' . $this->t("Add, delete, or update entity field descriptions from a CSV file. File must have a header row with the the following columns:") . '</p>';
+    $caption .= '<ul><li>"Entity type"</li><li>"Bundle machine ID"</li><li>"Field machine ID"</li><li>"Description"</li></ul>';
+    $caption .= "<p>Note that all changes are made 'in database' and must be exported as configuration changes for software control.";
+    $form['description'] = ['#markup' => $caption];
+
     $form['import_file'] = [
       '#type' => 'managed_file',
       '#name' => 'csv_import_file',
@@ -65,6 +71,21 @@ class FieldDescriptionsListImportForm extends FormBase {
       '#upload_validators' => $validators,
       '#upload_location' => 'public://',
       '#attributes' => ['class' => ['file-import-input']],
+    ];
+
+    // The wrapper for Ajax results list.
+    $form['import_summary'] = [
+      // Set the results to be below the form.
+      '#weight' => 100,
+      // The prefix/suffix are the div with the ID specified as the wrapper in
+      // the submit button's #ajax definition.
+      '#prefix' => '<div id="field_descriptions_list_import_summary_wrapper">',
+      '#suffix' => '</div>',
+      // The #markup element forces rendering of the #prefix and #suffix.
+      // Without content, the wrappers are not rendered. Therefore, an empty
+      // string is declared, ensuring that the wrapper for the search results
+      // is present when the page is loaded.
+      '#markup' => '',
     ];
 
     $form['actions']['#type'] = 'actions';
@@ -101,18 +122,39 @@ class FieldDescriptionsListImportForm extends FormBase {
         }
       }
 
+      $processed = $modified = $added = $deleted = 0;
+
       /* @var array $record */
       foreach ($records as $record) {
-        if (!$record['Description']) {
-          continue;
-        }
+        $processed++;
 
         /* @var \Drupal\field\FieldConfigInterface $field */
         $field = FieldConfig::loadByName($record['Entity type'], $record['Bundle machine ID'], $record['Field machine ID']);
 
         // If we found a field, set the description and save the field configuration in database.
         if ($field) {
-          $field->setDescription($record['Description']);
+          $existing = $field->getDescription();
+          $new = $record['Description'];
+
+          if (empty($existing) && empty($new)) {
+            continue;
+          }
+          elseif (empty($existing) && !empty($new)) {
+            $added++;
+          }
+          elseif (!empty($existing) && empty($new)) {
+            $deleted++;
+          }
+          elseif (!empty($existing) && !empty($new)) {
+            if (strcmp($existing, $new) == 0) {
+              continue;
+            }
+            else {
+              $modified++;
+            }
+          }
+
+          $field->setDescription($new);
           $field->save();
         }
       }
@@ -160,7 +202,7 @@ class FieldDescriptionsListImportForm extends FormBase {
    * @param int $id
    *   The File ID.
    *
-   * @return \Drupal\Core\Entity\EntityInterface|null
+   * @return EntityInterface|null
    *   The uploaded CSV file.
    *
    * @throws InvalidPluginDefinitionException
